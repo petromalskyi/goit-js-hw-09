c143167d9c059e92faf0d4e62e191cbda2f1df6e
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-12-17 13:38:29 +0200

    added styles on modal

--- src/js/gallery-img.js
+++ src/js/gallery-img.js
@@ -1,7 +1,7 @@
 export const images = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
